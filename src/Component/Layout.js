00299b55dc30145d393c5ebab52348a09de64487
Anant <anantpatel07012003@gmail.com>
2023-07-08 12:51:41 +0530

    user

--- src/Component/Layout.js
+++ src/Component/Layout.js
@@ -1,10 +1,28 @@
 import React from "react";
 import styles from "./Layout.module.css";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 export default function Layout() {
   return (
     <div className={styles.layout}>
-      <div className={styles.container1}></div>
-      <div className={styles.container1}></div>
+      <div className={styles.container1}> </div>
+      <div className={styles.container2}>
+        <div className={styles.userdetails}>
+          <div className={styles.welcome}>
+            <h3>Welcome back Isabella!</h3>
+            <p>Check Out today's Weather information</p>
+          </div>
+          <div className={styles.avatar}>
+            <MoreHorizIcon
+              className={styles.icon}
+              sx={{ transform: "scale(1.5)" }}
+            />
+            <img
+              src="https://img.freepik.com/free-photo/indoor-picture-cheerful-handsome-young-man-having-folded-hands-looking-directly-smiling-sincerely-wearing-casual-clothes_176532-10257.jpg?size=626&ext=jpg"
+              alt="user"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
