fa75aed2269a637f6b061f5f3b1d79aaf6a223f0
Anant <anantpatel07012003@gmail.com>
2023-07-08 12:05:45 +0530

    grocery

---
+++ src/Component/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Layout.module.css";
+export default function Layout() {
+  return (
+    <div className={styles.layout}>
+      <div className={styles.container1}></div>
+      <div className={styles.container1}></div>
+    </div>
+  );
+}
